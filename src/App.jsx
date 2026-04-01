667dfa9b65887698a2744261bfbae8dfce3a3dab
karina22032 <karina.salvi@zylitix.ai>
2026-04-01 11:51:58 +0530

    Initial-setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+return (
+  <div>
+    <h1 className="text-3xl font-bold underline text-center text-blue-500">
+      Hello world!
+    </h1>
+  </div>
+)
+}
+
+export default App
